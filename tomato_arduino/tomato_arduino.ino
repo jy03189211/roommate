e81e2fdd7d69d2ec58efb3685c68d1be1435d5a0
Jin <jin.jin@aalto.fi>
2016-03-11 22:52:36 +0200

    no message

--- tomato_arduino/tomato_arduino.ino
+++ tomato_arduino/tomato_arduino.ino
@@ -1,10 +1,11 @@
 
 #include <Ethernet.h>
 #include <SPI.h>
+#include <TinkerKit.h>
 #include "RestClient.h"
 
 RestClient client = RestClient("tomato-1230.herokuapp.com");
-
+TKLightSensor ldr(I1);	//create the "ldr" object on port I1
 //Setup
 void setup() {
   Serial.begin(9600);
@@ -23,10 +24,16 @@ void setup() {
 
 String response;
 void loop(){
+  int brightnessVal = ldr.read();  
   response = "";
-  char[] body="{/"room/":/"123/",/"motion_detected/":1,/"time/":/"2016-03-10T01:44:32.553210Z/"}"
+  char body[64];
+  sprintf(body, "{\"room\":\"123\",\"motion_detected\":1,\"time\":\"2016-03-10T01:44:32.553210Z\"}");
   int statusCode = client.post("/api/v1/motion/",body , &response);
+  Serial.print("Response from server: ");
+  Serial.println(response);
   Serial.print("Status code from server: ");
   Serial.println(statusCode);
+  Serial.print("brightness = " );                      
+  Serial.println(brightnessVal); 
   delay(1000);
 }

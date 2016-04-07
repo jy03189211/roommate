8809cea208ad1c48e478c150c79a78eefe158dfa
Julius Eerola <julius.eerola@aalto.fi>
2016-04-07 12:16:13 +0300

    Update cloud documentation

--- tomato_cloud/README.md
+++ tomato_cloud/README.md
@@ -5,7 +5,7 @@
 - Python `2.7.6`
 - Django `1.9.2`
 - Django REST framework `3.3.2`
-- Heroku
+- Heroku (http://tomato-1230.herokuapp.com)
 
 ```sh
 pip install virtualenv
@@ -25,11 +25,47 @@ python manage.py runserver
 
 ## Structure
 
-The Django project `tomato_cloud` has a root path `/api/v1/rooms`.
+The Django project `tomato_cloud` has a root path `/api/v1/`.
+
+### Rooms
+```sh
+//list rooms
+GET /api/v1/rooms
+
+//add new room
+POST /api/v1/rooms
+```
+
+### Room
+```sh
+//return room data by id
+GET /api/v1/rooms/<id>
+```
+
+### Sensors
+```sh
+//list sensors
+GET /api/v1/sensors
+
+//add new sensor
+POST /api/v1/sensors
+```
+
+### Sensor
+```sh
+//return sensor data
+GET /api/v1/sensors/<id>
+```
+
+### Sensor data
+```sh
+//return sensor data by room id and sensor type
+GET /api/v1/rooms/<room_id>/{motion,co2,temperature,humidity}
+```
 
 ## Heroku
 
 ```sh
-git subtree push--prefix tomato_cloud heroku master
+git subtree push --prefix tomato_cloud https://git.heroku.com/tomato-1230.git master
 ```
 
--- tomato_cloud/cloud_api/algorithms.py
+++ tomato_cloud/cloud_api/algorithms.py
@@ -1,4 +1,8 @@
 def update_room_status(room, sensor, validated_data):
+    '''Determine room availability based on the latest measurements.
+    Also, update the latest measurement data of the room object.
+    '''
+    
     if not (room and sensor and validated_data):
         print 'ERROR: no room | no sensor | validated_data'
         return
@@ -9,12 +13,23 @@ def update_room_status(room, sensor, validated_data):
         if detected == room.available:
             room.available = not detected
             room.save()
+            
+            #Push notification to Android
+            push_notification(room.available)
+    
     elif sensor.sensor_type == 'CO2':
         room.co2 = validated_data.get('concentration')
         room.save()
+        
     elif sensor.sensor_type == 'Temperature':
         room.temperature = validated_data.get('temperature')
         room.save()
+        
     elif sensor.sensor_type == 'Humidity':
         room.humidity = validated_data.get('humidity')
         room.save()
+
+def push_notification(available):
+    '''Push notification to Android when the status has changed.
+    '''
+    pass

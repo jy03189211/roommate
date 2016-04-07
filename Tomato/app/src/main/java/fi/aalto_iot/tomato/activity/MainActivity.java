9e3f40e82a7f28a5c73b22038db996aa4e2ef8d5
Juho Marttila <jma@Juhos-MacBook-Pro.local>
2016-04-07 16:39:37 +0300

    Primitive GCM registration to django

--- Tomato/app/src/main/java/fi/aalto_iot/tomato/activity/MainActivity.java
+++ Tomato/app/src/main/java/fi/aalto_iot/tomato/activity/MainActivity.java
@@ -61,6 +61,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         realm = Realm.getDefaultInstance();
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 

c0f5bcf41e9b154f5a820bf044fbc209e112f483
Julius Eerola <julius.eerola@aalto.fi>
2016-04-14 19:44:44 +0300

    Fix filter bug

--- tomato_cloud/cloud_api/views.py
+++ tomato_cloud/cloud_api/views.py
@@ -4,7 +4,6 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import detail_route
 from rest_framework import filters
-import filters as f
 
 import models, serializers
 

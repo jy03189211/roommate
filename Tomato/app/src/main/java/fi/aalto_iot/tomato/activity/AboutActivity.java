858dd6255c1f5409591c5704a550b8ed07935321
Julius Eerola <julius.eerola@aalto.fi>
2016-04-19 20:36:39 +0300

    Start android about activity

---
+++ Tomato/app/src/main/java/fi/aalto_iot/tomato/activity/AboutActivity.java
@@ -0,0 +1,17 @@
+package fi.aalto_iot.tomato.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.TextView;
+
+import fi.aalto_iot.tomato.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
